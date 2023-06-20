0b53436f4248831dbea0ba98b02e281a2eed8f18
cianyi <18898691409@163.com>
2023-06-20 10:10:47 +0800

    上传项目

---
+++ src/A.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cianyi
+ * Date: 2023/6/20
+ * Time: 10:06
+ */
+namespace Cianyi\Tp6flowrestrrictions;
+
+class A
+{
+    public function abc()
+    {
+        dd(123456);
+    }
+}
